fade64e102f6b8c99c4de771fb1c7ad7b9e86831
adrian abadin <aabadin@gmail.com>
2023-08-26 14:58:22 -0300

    Desafio Login Refactor completo

--- src/auth/auth.passport.controller.ts
+++ src/auth/auth.passport.controller.ts
@@ -5,10 +5,23 @@ import { DoneCallback } from "passport"
 const passport= new PassportAuthService()
 export class PassportController {
     constructor (
-        private passportService=passport
+        private passportService= new PassportAuthService()
     ){}
     async localLogin(req:Request,username:string,password:string,done:(error:any,data:any,...args:any)=>any){
-
+try{
+        const data = await passport.findByUserName(username)
+        if (data.ok){
+            const response = data.data
+            if (response !== null&& typeof response ==="object" && "password" in response){
+                if (await argon.verify(response.password,password)){
+                    return done(null,response)
+                }else throw new Error ("Wrong Password")
+            }else throw new Error("User not found")
+        }else throw new Error("User not found!")
+}catch(e){
+    console.log(e)
+    return done(e,null)
+}
     }
     async localSignUp(req:Request,username:string,password:string,done:(error:any,data:any,...args:any)=>any){
         try{
@@ -28,12 +41,12 @@ export class PassportController {
             }
         }catch(e){
             console.log(e)
-
+return done(e,null)
 
         }
     }
     async serialize(user:any,done:DoneCallback){
-        done(null,user.id)
+        done(null,user._id)
     }
     async deSerialize(userId:string,done:DoneCallback){
         const data= await this.passportService.findUserById(userId)
@@ -42,7 +55,28 @@ export class PassportController {
         }else done(data.error,null)
     }
     async gitHubLogin(accesstoken:string,refreshtoken:string,profile:any,cb:DoneCallback){
-    
-        console.log(profile["_json"])
+        try{
+ 
+            if (profile !== null &&typeof profile =="object" && "_json" in profile && "email" in profile._json) {
+                const username = profile["_json"].email as string
+                const data = await passport.findByUserName(username.toLowerCase())
+                console.log(data,username)
+                if (data.ok){
+                    const response = data.data
+                    if (response !== null && typeof response ==="object" && "_id" in response){
+                        return cb(null,response)
+
+                    } else{
+                        throw new Error("User not found!")
+                    }
+                }else throw new Error("User not found!")
+            }else throw new Error("User not Found!")
+            
+
+        }catch(e){
+            console.log(e)
+            return cb(e,null)
+
+        }
     }
 }
